3db566f76cc55b64826a65adc97f6b9d6a7762b3
KalleSjostrom <kalle.sjostrom@arrowheadgs.com>
2014-01-27 16:59:57 +0100

    Added a basic hud with placeholder info.
    Added a hud class, font and gui skins to handle the drawing of the hud.

---
+++ Assets/Scripts/Gui/Hud.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Hud : MonoBehaviour {
+
+	public GUISkin skin;
+
+	private int mLevel = 1;
+	private int mScore = 0;
+
+	private enum InfoType {
+		Level = 0,
+		Score = 1,
+	}
+
+	private Dictionary<InfoType, Rect> mLayout;
+
+	// Use this for initialization
+	void Awake() {
+		mLayout = new Dictionary<InfoType, Rect>();
+		int x = 40;
+		int y = 40;
+		int height = 25;
+		int width = 250;
+
+		int nrElements = Enum.GetNames(typeof(InfoType)).Length;
+		for (int i = 0; i < nrElements; i++) {
+			mLayout.Add((InfoType) i, new Rect(x, y + i * height, width, height));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	}
+
+	void OnGUI() {
+		GUI.skin = skin;
+
+		GUI.Label(mLayout[InfoType.Level], String.Format("Level: {0}", mLevel));
+		GUI.Label(mLayout[InfoType.Score], String.Format("Score: {0}", mScore));
+	}
+}
