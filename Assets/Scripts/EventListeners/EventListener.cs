cf641d21b23ee889c1fc806caaf9cf7ea47b421b
Kalle Sjöström <c06ksm@gmail.com>
2014-01-29 20:30:35 +0100

    Added event listeners and a button tile.
    A button tile will trigger an event on all event listeners specified, (could be on several other objects). The event listener will the pass the message on to other scripts by Component.SendMessage or on certain events in might assume the existence of a given component.
    
    Signed-off-by: Kalle Sjöström <c06ksm@gmail.com>

---
+++ Assets/Scripts/EventListeners/EventListener.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum EventMessage {
+	Activate,
+	Deactivate,
+	ToggleActivate,
+}
+
+public class EventListener : MonoBehaviour {
+
+	public void ReceiveEvent(EventMessage iEvent) {
+		switch (iEvent) {
+		case EventMessage.Activate : 
+			SendMessage("SetActive", true);
+			break;
+		case EventMessage.Deactivate :
+			SendMessage("SetActive", false);
+			break;
+		case EventMessage.ToggleActivate :
+			MonoBehaviour[] mb = GetComponents<MonoBehaviour>();
+			foreach (MonoBehaviour b in mb) {
+				if (b is IActivatable) {
+					IActivatable a = (b as IActivatable);
+					a.SetActive(!a.IsActive());
+				}
+			}
+			break;
+		}
+	}
+}
---
+++ Assets/Scripts/Tiles/ButtonTile.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonTile : BaseTile 
+{
+	public EventListener[] ObjectsToNotify;
+	public EventMessage Message;
+	
+	protected override void OnLeaved(BaseUnit iUnit, BaseTile iNextTile) {}
+	protected override void OnArrived(BaseUnit iUnit, BaseTile iPreviousTile) {
+		foreach (EventListener el in ObjectsToNotify)
+			el.ReceiveEvent(Message);
+	}
+}
