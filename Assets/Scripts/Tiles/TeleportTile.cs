b7f6d0bea8b384c3345e55c41ca2a73feddb4196
Kalle Sjöström <c06ksm@gmail.com>
2014-01-29 07:52:22 +0100

    Added a simple teleport floor tile.
    It selects a random tile in its OnArrived and sends the avatar there.
    
    Signed-off-by: Kalle Sjöström <c06ksm@gmail.com>

---
+++ Assets/Scripts/Tiles/TeleportTile.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class TeleportTile : BaseTile 
+{
+	protected override void OnLeaved(BaseUnit iUnit) {}
+	protected override void OnArrived(BaseUnit iUnit) {
+		BaseTile tile = null;
+		while(tile == null) {
+			int x = Random.Range(0, GridManager.GetLength(0));
+			int y = Random.Range(0, GridManager.GetLength(1));
+			tile = GridManager.GetTile(x, y);
+			if (tile != null && (tile == this || tile.Occupied)) // This should be solved better... A "is tile valid" check..
+				tile = null;
+		}
+		Vector3 position = tile.transform.position;
+		position.y = 1; // This should be solved better... Like "place unit on tile" function.
+		iUnit.transform.position = position;
+		tile.Occupy(iUnit);
+	}
+}
