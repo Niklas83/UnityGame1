16683fcc3258a4506403eb54618a05f1da39bd1d
Niklas83 <niklas_ronnlund@hotmail.com>
2014-02-23 20:32:14 +0100

    ProjectileShooter done :)
    Created projectile shooter

---
+++ Assets/Scripts/Enums/DirectionEnum.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum DirectionEnum {
+    Up,
+    Down,
+    Left,
+    Right
+}
---
+++ Assets/Scripts/Units/ProjectileShooterUnit.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public sealed class ProjectileShooterUnit : BaseUnit
+{
+    //public fields
+    public bool IsActive = true;            //Sets if the cannon is active (Could be disabled or something by walking on button etc)
+    public float SecondsBetweenShots = 5;
+    public float ProjectileSpeed = 4;
+    public Vector3 PoisitionToSpawnTheProjectile;    //from where the projectile will be spawned
+    public ProjectileTypeEnum ProjectileType;       //the type of projectile being launched from the cannon
+    public DirectionEnum DirectionToShoot;      //The direction to shoot the projectile
+    public bool CanShootThroughWalls = false;           //TODO: Must make it so you can have multiple units on each floor tile, arraylist connected to each tile for example
+    public bool ResetMapIfPlayerSlain = false;
+
+    //ONE of the below Prefab to use based on what set ProjectileType
+    public GameObject FireBallPrefab;
+    public GameObject ArrowPrefab;
+    public GameObject IceBallPrefab;
+    public GameObject SpearPrefab;
+    public GameObject ToxicBallPrefab;
+
+    private bool IsCurrentlyShooting = false;
+
+    public override bool CanWalkOn
+    {
+        get
+        {
+            return false;
+        }
+    }
+
+    void Update()
+    {
+        if (IsActive && !IsCurrentlyShooting)
+        {
+            IsCurrentlyShooting = true;
+            StartCoroutine(StartShooting());
+        }
+    }
+
+    IEnumerator StartShooting()
+    {
+        while (IsActive)
+        {
+            Shoot();
+
+            yield return new WaitForSeconds(SecondsBetweenShots);
+        }
+        IsCurrentlyShooting = false;
+    }
+
+
+    void Shoot()
+    {
+        switch (DirectionToShoot)       //TODO:If we want to keep the spawning within the statue this can be removed this switch can be removed
+        {
+                //If direction to shot is set down (z-1)
+            case DirectionEnum.Down:
+                PoisitionToSpawnTheProjectile = new Vector3(
+                    this.gameObject.transform.position.x,
+                    this.gameObject.transform.position.y+1,
+                    this.gameObject.transform.position.z);
+                break;
+                //If direction to shot is set down (x-1)
+            case DirectionEnum.Left:
+                PoisitionToSpawnTheProjectile = new Vector3(
+                    this.gameObject.transform.position.x,
+                    this.gameObject.transform.position.y + 1,
+                    this.gameObject.transform.position.z);
+                break;
+                //If direction to shot is set down (x+1)
+            case DirectionEnum.Right:
+                PoisitionToSpawnTheProjectile = new Vector3(
+                    this.gameObject.transform.position.x,
+                    this.gameObject.transform.position.y + 1,
+                    this.gameObject.transform.position.z);
+                break;
+                //If direction to shot is set down (z+1)
+            case DirectionEnum.Up:
+                PoisitionToSpawnTheProjectile = new Vector3(
+                    this.gameObject.transform.position.x,
+                    this.gameObject.transform.position.y + 1,
+                    this.gameObject.transform.position.z);
+                break;
+
+        }
+        
+        var xDirection = 0;
+        var zDirection = 0;
+        //Direction to apply "force"
+        switch (DirectionToShoot)
+        {
+            case DirectionEnum.Down:
+                zDirection = -1;
+                break;
+            case DirectionEnum.Left:
+                xDirection = -1;
+                break;
+            case DirectionEnum.Right:
+                xDirection = 1;
+                break;
+            case DirectionEnum.Up:
+                zDirection = 1;
+                break;
+        }
+
+        var projectile = new GameObject();     //Temp value that will be set below 
+
+        //Checks if its gonna shoot a fireball
+        if (ProjectileType == ProjectileTypeEnum.FireBall)
+        {
+             projectile = (GameObject)Instantiate(FireBallPrefab, PoisitionToSpawnTheProjectile, Quaternion.identity);
+        }
+        //Checks if its gonna shoot a iceball
+        if (ProjectileType == ProjectileTypeEnum.IceBall)
+        {
+            projectile = (GameObject)Instantiate(IceBallPrefab, PoisitionToSpawnTheProjectile, Quaternion.identity);
+        }
+        //Checks if its gonna shoot a toxicball
+        if (ProjectileType == ProjectileTypeEnum.ToxicBall)
+        {
+            projectile = (GameObject)Instantiate(ToxicBallPrefab, PoisitionToSpawnTheProjectile, Quaternion.identity);
+        }
+
+        var projectileGameObject = projectile;
+        ProjectileMover projectileMover = projectileGameObject.GetComponent<ProjectileMover>();
+
+        if (projectileMover != null)
+        {
+            projectileMover.init();
+            projectileMover.MoveSpeed = ProjectileSpeed;
+            projectileMover.CanPassThroughUnits = CanShootThroughWalls;
+            projectileMover.ResetMapIfPlayerIsKilled = ResetMapIfPlayerSlain;
+            projectileMover.xDir = xDirection;
+            projectileMover.yDir = zDirection;
+
+            projectileMover.TryMove();  //Instantierar flygvägen för projektilen 
+        }
+        
+    }
+
+
+}
