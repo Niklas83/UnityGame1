a8a0f1a2bcbf840068439af4d51f7326204962b0
Kalle Sjöström <c06ksm@gmail.com>
2014-03-15 19:39:42 +0100

    Added a LaserBeamer that shoots a constant laser beam that reflects on stuff. Right now, it only sends a message (HitByLaser) to the hit game object.

---
+++ Assets/Scripts/GameEntities/Units/LaserBeamUnit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class LaserBeamUnit : BaseUnit
+{
+	public override int LayerMask { get { return (int)(Layer.Air | Layer.Ground); } }
+	public override bool CanWalkOver { get { return false; } }
+	public override bool CanWalkOn(string incomingUnitTag) { return CanWalkOver; }
+
+    public void Start() {
+		// LaserBeam = GetComponentInChildren<LaserBeam>();
+	}
+}
---
+++ Assets/Scripts/LaserBeamHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LaserBeamHandler : MonoBehaviour
+{
+	public GameObject LaserBeamPrefab;
+	private List<LineRenderer> beamCache;
+
+    public void Start() {
+		beamCache = new List<LineRenderer>();
+		GetBeam(0);
+	}
+
+	private LineRenderer GetBeam(int index) {
+		LineRenderer lr = null;
+		if (index >= beamCache.Count) {
+			GameObject go = GameObject.Instantiate(LaserBeamPrefab) as GameObject;
+			go.transform.parent = transform.parent;
+			go.transform.localPosition = Vector3.zero;
+			lr = go.GetComponent<LineRenderer>();
+			beamCache.Add(lr);
+		} else
+			lr = beamCache[index];
+
+		return lr;
+	}
+	
+	void Update() {
+		foreach(LineRenderer lr in beamCache)
+			lr.enabled = false;
+
+		Vector3 forward = transform.rotation * Vector3.forward;
+		ShootBeam(transform, forward, 0);
+	}
+
+	public void ShootBeam(Transform t, Vector3 direction, int index) {
+		Vector3 position = t.position;
+
+		if (index > 100)
+			return;
+
+		LineRenderer lr = GetBeam(index);
+		lr.enabled = true;
+		lr.SetPosition(0, position);
+		
+		RaycastHit hit;
+		if (Physics.Raycast(position, direction, out hit)) {
+			Transform hitTrans = hit.collider.transform;
+
+			direction = Vector3.Reflect(direction, hit.normal); // hitTrans.rotation * Vector3.forward;
+			lr.SetPosition(1, hitTrans.position);
+
+			hitTrans.SendMessage("HitByLaser", this, SendMessageOptions.DontRequireReceiver);
+			ShootBeam(hitTrans, direction, index+1);
+		} else {
+			lr.SetPosition(1, position + direction * 50);
+		}
+	}
+}
