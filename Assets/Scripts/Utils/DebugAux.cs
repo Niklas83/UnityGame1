a8f1e7e54ed1e31389663d30bda43502ced8e960
Kalle Sjöström <c06ksm@gmail.com>
2014-04-15 10:40:56 +0200

    Added a static Log class that prints formatted log strings.

--- Assets/Scripts/Utils/DebugAux.cs
+++ Assets/Scripts/Utils/DebugAux.cs
@@ -10,3 +10,12 @@ public static class DebugAux
 	}
 }
 
+public static class Log
+{
+	public static void debug(string category, string format, params object[] args) 
+	{
+		string s = String.Format(format, args);
+		s = String.Format("[{0}] [{1}] {2}", DateTime.Now.ToString(), category, s);
+		Debug.Log(s);
+	}
+}
