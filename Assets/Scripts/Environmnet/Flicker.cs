95c7eaae358c38301d0cd5913ce6edff382e362a
Kalle Sjöström <c06ksm@gmail.com>
2014-04-17 10:06:57 +0200

    Added a bit of flickering to the burning candle flame.

---
+++ Assets/Scripts/Environmnet/Flicker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flicker : MonoBehaviour {
+
+	public float speed = 2;
+	public float amplitude = 0.1f;
+
+	private Vector3 _originalPosition;
+	private float _seed;
+
+	void Start () {
+		_originalPosition = transform.position;
+		_seed = Random.value * 10;
+	}
+	
+	void Update () {
+		float heightX = Mathf.PerlinNoise(Time.time * speed, _seed);
+		float x = (heightX - 0.5f) * amplitude;
+		
+		float heightZ = Mathf.PerlinNoise(Time.time * speed, _seed*2);
+		float z = (heightZ - 0.5f) * amplitude;
+		transform.position = _originalPosition + new Vector3(x, 0, z);
+	}
+}
