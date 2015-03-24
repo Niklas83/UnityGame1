34c6eb0e5f0a875da06df96131cf525e6691c280
Niklas83 <niklas_ronnlund@hotmail.com>
2015-03-24 22:32:35 +0100

    Rotate Random on replace prefab tool

--- Assets/Editor/ReplacePrefab.cs
+++ Assets/Editor/ReplacePrefab.cs
@@ -12,6 +12,8 @@ public class ReplaceGameObjects : ScriptableWizard
 	public bool copyValues = true;
 	public GameObject NewType;
 	public GameObject[] OldObjects;
+
+    public bool SetRandomRotation = true;
 	
 	[MenuItem("Custom/Replace GameObjects")]
 	static void CreateWizard()
@@ -24,16 +26,50 @@ public class ReplaceGameObjects : ScriptableWizard
         //Transform[] Replaces;
         //Replaces = Replace.GetComponentsInChildren<Transform>();
 
+        
+
         foreach (GameObject go in OldObjects)
         {
             GameObject newObject;
 
             newObject = (GameObject)PrefabUtility.InstantiatePrefab(NewType);
             newObject.transform.position = go.transform.position;
-            newObject.transform.rotation = go.transform.rotation;
             newObject.transform.parent = go.transform.parent;
-
+            if (SetRandomRotation)
+            {
+                newObject.transform.Rotate(0, GetRandomAxisRotation(), 0);
+            }
+            else
+            {
+                newObject.transform.rotation = go.transform.rotation;
+            }
             DestroyImmediate(go);
+        }
+    }
+
+    private float GetRandomAxisRotation()
+    {
+        int axis = Random.Range(1, 5);
+
+        if (axis == 1)
+        {
+            return 0f;
+        }
+        else if (axis == 2)
+        {
+            return 90f;
+        }
+        else if (axis == 3)
+        {
+            return 270f;
+        }
+        else if (axis == 4)
+        {
+            return 0f;
+        }
+        else
+        {
+            return 0;
         }
     }
 }
