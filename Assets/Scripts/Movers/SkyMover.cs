bd863cb0db5a2fd7988dba93c08fef2827d6d796
Niklas83 <niklas_ronnlund@hotmail.com>
2015-04-18 16:51:12 +0200

    Moving sky functionallity + upped resolution on a few of the sky boxes

---
+++ Assets/Scripts/Movers/SkyMover.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyMover : MonoBehaviour
+{
+
+    public float XRotationSpeed = 0.02f;
+
+    public float YRotationSpeed = 0.02f;
+
+    public float ZRotationSpeed = 0.02f;
+
+    private Transform _trans;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _trans = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    RotateSky();
+	}
+
+
+    public void RotateSky()
+    {
+        _trans.Rotate(XRotationSpeed, YRotationSpeed, ZRotationSpeed);
+    }
+
+
+}
