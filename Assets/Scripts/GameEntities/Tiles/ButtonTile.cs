64b5d1465055af7966eb0cf56c8f64c207a14fc1
Niklas83 <niklas_ronnlund@hotmail.com>
2015-06-18 21:34:41 +0200

    Added so u can have 2 lists of items with a set of methods for each list

--- Assets/Scripts/GameEntities/Tiles/ButtonTile.cs
+++ Assets/Scripts/GameEntities/Tiles/ButtonTile.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 using System.Collections;
 
 public class ButtonTile : BaseTile 
@@ -21,46 +22,68 @@ public class ButtonTile : BaseTile
     public override BaseTile TeleportDestinationTile { get { return DestinationTeleportTile; }}
     public BaseTile DestinationTeleportTile;
 
-	public EventListener[] objectsToNotify;
-	
     public EventMessage ArriveMessage;
 
     public EventMessage LeaveMessage;
 
-    //sätt denna till true
+    //sätt denna till true för att använda "statiska event metoder"
     public bool UseStaticEventMethods = true;
 
-    public StaticEventMethods StaticArriveMethod;
-    public StaticEventMethods StaticLeaveMethod;
+    //Object Setup 1
+    public EventListener[] objectsToNotify_1;
+    public StaticEventMethods StaticArriveMethod_1;
+    public StaticEventMethods StaticLeaveMethod_1;
+
+    //Object Setup 2
+    public EventListener[] objectsToNotify_2;
+    public StaticEventMethods StaticArriveMethod_2;
+    public StaticEventMethods StaticLeaveMethod_2;
+
+
+
+    protected override void OnArrived(BaseUnit unit, BaseTile previousTile)
+    {
+        base.OnArrived(unit, previousTile);
+
+        if (UseStaticEventMethods && (objectsToNotify_1.Any() || objectsToNotify_2.Any()))
+        {
+            foreach (EventListener el in objectsToNotify_1)
+            {
+                el.ReceiveEventMethod(StaticArriveMethod_1, this.EventGetGridManager()); //Sends the gridmanager for objects who was not active at game start
+            }
+            foreach (EventListener el in objectsToNotify_2)
+            {
+                el.ReceiveEventMethod(StaticArriveMethod_2, this.EventGetGridManager()); //Sends the gridmanager for objects who was not active at game start
+            }
+        }
+        else
+        {
+            foreach (EventListener el in objectsToNotify_1)
+                el.ReceiveEvent(ArriveMessage);
+        }
+    }
 
     protected override void OnLeaved(BaseUnit unit, BaseTile nextTile)
     {
-        if (UseStaticEventMethods)
+        if (UseStaticEventMethods && (objectsToNotify_1.Any() || objectsToNotify_2.Any()))
         {
-            foreach (EventListener el in objectsToNotify)
-                el.ReceiveEventMethod(StaticLeaveMethod, this.EventGetGridManager());   //Sends the gridmanager for objects who was not active at game start
+            foreach (EventListener el in objectsToNotify_1)
+            {
+                el.ReceiveEventMethod(StaticLeaveMethod_1, this.EventGetGridManager()); //Sends the gridmanager for objects who was not active at game start
+            }
+            foreach (EventListener el in objectsToNotify_2)
+            {
+                el.ReceiveEventMethod(StaticLeaveMethod_2, this.EventGetGridManager()); //Sends the gridmanager for objects who was not active at game start          
+            }
         }
 
         else
         {
-            foreach (EventListener el in objectsToNotify)
+            foreach (EventListener el in objectsToNotify_1)
                 el.ReceiveEvent(LeaveMessage);
         }
     }
 
 
-	protected override void OnArrived(BaseUnit unit, BaseTile previousTile) {
-        base.OnArrived(unit, previousTile);
 
-	    if (UseStaticEventMethods)
-	    {
-            foreach (EventListener el in objectsToNotify)
-                el.ReceiveEventMethod(StaticArriveMethod, this.EventGetGridManager());   //Sends the gridmanager for objects who was not active at game start
-	    }
-	    else
-	    {
-            foreach (EventListener el in objectsToNotify)
-                el.ReceiveEvent(ArriveMessage);
-	    }
-	}
 }
